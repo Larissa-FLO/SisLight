48fab9ef217d68d0ec60f9192c3d785f9862cdab
tarde.cetafaju <tarde.cetafaju@PC03LAB2929>
2024-05-02 17:06:08 -0300

    Adicionar arquivos de projeto.

---
+++ SisLight/Lampadas/Lampada.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisLight.Lampadas
+{
+    public class Lampada
+    {
+        public int Codigo { get; set; }
+        public int Watts { get; set; }
+        public bool Ligada = false;
+        public static int UltimoCodigo = 1;
+
+        public static List<Lampada> Lampadas = new();
+
+        public static void LigarLampada(int codigo)
+        {
+            if (Lampadas.Count > 0)
+            {
+                foreach (var lampada in Lampadas)
+                {
+                    if (lampada.Codigo == codigo && lampada.Ligada == true)
+                    {
+                        Console.WriteLine("A lâmpada já está ligada.");
+                    }
+                    else if (lampada.Codigo == codigo && lampada.Ligada == false)
+                    {
+                        lampada.Ligada = true;
+                        Console.WriteLine("A lâmpada foi ligada.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Código inválido. Tente novamente.");
+            }
+        }
+
+        public static void DesligarLampada(int codigo)
+        {
+            if (Lampadas.Count > 0)
+            {
+                foreach (var lampada in Lampadas)
+                {
+                    if (lampada.Codigo == codigo && lampada.Ligada == false)
+                    {
+                        lampada.Ligada = true;
+                        Console.WriteLine("A lâmpada já está desligada.");
+                    }
+                    else if (lampada.Codigo == codigo && lampada.Ligada == true)
+                    {
+                        lampada.Ligada = false;
+                        Console.WriteLine("A lâmpada foi desligada.");
+                    }
+                } 
+            }
+            else
+            {
+                Console.WriteLine("Código inválido. Tente novamente.");
+            }
+        }
+
+        public static void CadastrarLampada()
+        {
+            int i = 0;
+           
+            while (i < 5)
+            {
+                Lampada lampada = new Lampada();
+                lampada.Codigo = UltimoCodigo;
+
+                Console.WriteLine($"Código: {lampada.Codigo} \nSituação da lâmpada: Desligada.");
+
+                Console.WriteLine("Informe a potência da lâmpada:");
+                int watts = Convert.ToInt32(Console.ReadLine());
+                lampada.Watts = watts;
+
+                Console.WriteLine("Lâmpada cadastrada com sucesso.");
+                Lampadas.Add(lampada);
+                UltimoCodigo++;
+                i++;
+            }
+        }
+
+        public static void ConsultarPotencia()
+        {
+            int totalWatts = 0;
+
+            foreach (var lampada in Lampadas)
+            {
+                if (lampada.Ligada == true)
+                {
+                    totalWatts += lampada.Watts;
+                }
+            }
+            if (totalWatts == 0)
+            {
+                Console.WriteLine("Não há nenhuma lâmpada acesa no momento.");
+            }
+            else
+            {
+                Console.WriteLine($"O total de potência utilizada é de {totalWatts} watts.");
+            }
+        }
+    }
+}
---
+++ SisLight/Program.cs
@@ -0,0 +1,48 @@
+﻿using SisLight.Lampadas;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SisLight.Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MenuLampadas();
+        }
+
+        public static void MenuLampadas()
+        {
+            bool loop = true;
+
+            while (loop == true)
+            {
+                Console.WriteLine("Selecione a opção desejada: \n1 – Cadastrar lâmpadas\r\n2 – Ligar lâmpada\r\n3 – Desligar lâmpada\r\n4 – Consultar potência utilizada\r\n5 – Sair");
+                int opt = Convert.ToInt32(Console.ReadLine());
+
+                switch (opt)
+                {
+                    case 1:
+                        Lampada.CadastrarLampada();
+                        break;
+                    case 2:
+                        Console.WriteLine("Insira o código da lâmpada que deseja ligar:");
+                        int codigo1 = Convert.ToInt32(Console.ReadLine());
+                        Lampada.LigarLampada(codigo1);
+                        break;
+                    case 3:
+                        Console.WriteLine("Insira o código da lâmpada que deseja desligar:");
+                        int codigo2 = Convert.ToInt32(Console.ReadLine());
+                        Lampada.DesligarLampada(codigo2);
+                        break;
+                    case 4:
+                        Lampada.ConsultarPotencia();
+                        break;
+                    case 5:
+                        loop = false;
+                        break;
+                }
+            }
+
+        }
+    }
+}
